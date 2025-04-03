7314a4be9da6704755988910da3d52eb2d1c5cb0
yuxin <uxinjian@gmail.com>
2025-04-04 08:40:22 +1000

    fix: backend api scheme

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // by using defined backend api ip and port
-  baseURL: 'https://3.104.106.154:5001',
+  baseURL: 'http://3.104.106.154:5001',
   headers: { 'Content-Type': 'application/json' },
 });
 
